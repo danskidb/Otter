1058c16c780fb7b3745f8c773f600c8179f50226
Danny de Bruijne <dannydb44@gmail.com>
2021-02-02 22:52:44 +0000

    Significantly improve logging

--- Source/Otter/Log.cpp
+++ Source/Otter/Log.cpp
@@ -8,11 +8,11 @@ namespace Otter {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("Otter");
+		spdlog::set_pattern("%^[%T][%L]%n%v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("[Otter]");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("App");
+		s_ClientLogger = spdlog::stdout_color_mt("[App]");
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 
@@ -24,4 +24,12 @@ namespace Otter {
 			assert(condition);
 		}
 	}
+
+	std::string Log::GetFileName(const char filePath[])
+	{
+		std::string toReturn = std::string(filePath);
+		std::size_t found = toReturn.find_last_of("/\\");
+		return toReturn.substr(found+1);
+	}
+
 }
--- Source/Otter/Log.h
+++ Source/Otter/Log.h
@@ -7,6 +7,7 @@
 #pragma warning(push, 0)
 #include "spdlog/spdlog.h"
 #pragma warning(pop) 
+#include <string>
 
 namespace Otter {
 
@@ -14,11 +15,12 @@ namespace Otter {
 	{
 	public:
 		static void Init();
-
 		static void CoreAssert(bool condition, std::string errorMsg);
+		static std::string GetFileName(const char filePath[]);
 
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
@@ -27,16 +29,16 @@ namespace Otter {
 }
 
 // Core log macros
-#define OT_CORE_TRACE(...)    ::Otter::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define OT_CORE_INFO(...)     ::Otter::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define OT_CORE_WARN(...)     ::Otter::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define OT_CORE_ERROR(...)    ::Otter::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define OT_CORE_FATAL(...)    ::Otter::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define OT_CORE_TRACE(...)    ::Otter::Log::GetCoreLogger()->trace("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_CORE_INFO(...)     ::Otter::Log::GetCoreLogger()->info("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_CORE_WARN(...)     ::Otter::Log::GetCoreLogger()->warn("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_CORE_ERROR(...)    ::Otter::Log::GetCoreLogger()->error("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_CORE_FATAL(...)    ::Otter::Log::GetCoreLogger()->fatal("[{}] {}", __FUNCTION__, __VA_ARGS__)
 #define OT_ASSERT(condition, errorMsg) ::Otter::Log::CoreAssert(condition, errorMsg)
 
 // Client log macros
-#define OT_TRACE(...)	      ::Otter::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define OT_INFO(...)	      ::Otter::Log::GetClientLogger()->info(__VA_ARGS__)
-#define OT_WARN(...)	      ::Otter::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define OT_ERROR(...)	      ::Otter::Log::GetClientLogger()->error(__VA_ARGS__)
-#define OT_FATAL(...)	      ::Otter::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define OT_TRACE(...)	      ::Otter::Log::GetClientLogger()->trace("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_INFO(...)	      ::Otter::Log::GetClientLogger()->info("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_WARN(...)	      ::Otter::Log::GetClientLogger()->warn("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_ERROR(...)	      ::Otter::Log::GetClientLogger()->error("[{}] {}", __FUNCTION__, __VA_ARGS__)
+#define OT_FATAL(...)	      ::Otter::Log::GetClientLogger()->fatal("[{}] {}", __FUNCTION__, __VA_ARGS__)
