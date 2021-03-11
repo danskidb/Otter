45d57747d7d56637ebc66067171bf8dcbbf9e857
Danny de Bruijne <dannydb44@gmail.com>
2021-03-11 15:59:46 +0000

    List: Add Pop, Remove and Insert functions

--- Source/Otter/Types/OList.h
+++ Source/Otter/Types/OList.h
@@ -31,6 +31,7 @@ namespace Otter {
 			return content[index];
 		}
 
+		// Copy a new element to the end of the list
 		inline void Push(const T& toAppend)
 		{
 			if (length >= allocated)
@@ -40,6 +41,7 @@ namespace Otter {
 			length++;
 		}
 
+		// Move a new element to the end of the list
 		inline void Push(T&& toAppend)
 		{
 			if (length >= allocated)
@@ -49,7 +51,81 @@ namespace Otter {
 			length++;
 		}
 
-		// Variadric template
+		// remove the last element from the list and return it (with move)
+		// Not quite sure yet whether this will work well - std::move() will copy if a move constructor is not available, so we may end up with a memory leak?
+		inline T&& Pop()
+		{
+			return std::move(content[--length]);
+		}
+
+		// removes an item from the array, keeps the allocated memory size.
+		inline void Remove(size_t index)
+		{
+			if (index < 0 || index >= length)
+			{
+				OT_WARN("Failed to insert object as the provided index was out of range.");
+				return;
+			}
+
+			T* newAllocated = new T[allocated];
+			for (size_t i = 0; i < length; i++)
+			{
+				if (i == index)	// do not move the element we are going to delete.
+					continue;
+
+				size_t newIndex = i;
+				if (i > index)
+					newIndex = i - 1;
+
+				newAllocated[newIndex] = std::move(content[i]);
+			}
+
+			length--;
+			delete[] content;
+			content = newAllocated;
+		}
+
+		inline void Insert(const T& toInsert, size_t index)
+		{
+			if (index == length)
+			{
+				Push(std::move(toInsert));
+				return;
+			}
+			
+			if (index < 0 || index >= length)
+			{
+				OT_WARN("Failed to insert object as the provided index was out of range.");
+				return;
+			}
+
+			size_t newAllocatedLength = allocated;
+			if (length + 1 >= allocated)
+				newAllocatedLength++;
+
+
+			T* newAllocated = new T[newAllocatedLength];
+			for (size_t i = 0; i < length; i++)
+			{
+				if (i == index)
+				{
+					newAllocated[i] = toInsert;
+				}
+
+				size_t newIndex = i;
+				if (i >= index)
+					newIndex = i + 1;
+
+				newAllocated[newIndex] = std::move(content[i]);
+			}
+
+			length++;
+			delete[] content;
+			content = newAllocated;
+			allocated = newAllocatedLength;
+		}
+
+		// Variadric template, construct a new object in the list in-place.
 		template<typename... Args>
 		inline T& Emplace(Args&&... args)
 		{
@@ -61,10 +137,11 @@ namespace Otter {
 			// we could leave out the lvalue since unused but this should make the syntax a bit more recognisable.
 			// Alternatively you can write "content[length] = T(std::forward<Args>(args)...);" but that makes the function just a glorified Pushback, since you end up calling the move constructor anyways.
 			T* emplaced = new(&content[length]) T(std::forward<Args>(args)...);
-			return content[length - 1];
+			return content[length++];
 		}
 
-		inline void Reserve(size_t newLength) { Reallocate(newLength); }
+		inline void Reserve(size_t newLength) { Reallocate(newLength); }	// Reserve memory for x entries
+		inline void Compress() { Reallocate(length); }						// Can be used when removing a lot of entries, frees up unused memory.
 		inline const bool HasUnusedCapacity() { return length < allocated; }
 		inline const bool IsEmpty() { return length <= 0; }
 		inline const size_t Length() { return length; }
